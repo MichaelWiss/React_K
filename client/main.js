b46d604bbf65851bebce45e928e3415108bf5042
MichaelWiss <michael.wiss@gmail.com>
2017-01-26 16:31:43 -0600

    axios get request

--- client/main.js
+++ client/main.js
@@ -3,7 +3,9 @@
 //Import the react library
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import ImageList from './components/image_list';
+
 
 //create a component
 const App = () => {
@@ -18,6 +20,8 @@ const App = () => {
 //render this component to the screen
 Meteor.startup(() => {
    ReactDOM.render(<App />, document.querySelector('.container'));
+   axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
+   .then(response => console.log(response));
 });
 
 
