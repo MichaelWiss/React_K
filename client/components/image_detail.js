fb61c8e54004c7ff5289953a82f645be9d548075
MichaelWiss <michael.wiss@gmail.com>
2017-01-26 14:30:46 -0600

    added bootstrap css to meteor

--- client/components/image_detail.js
+++ client/components/image_detail.js
@@ -6,9 +6,15 @@ const ImageDetail = (props) => {
 // props.image.link
 
 	return (
-		<li>
+		<li className="media list-group-item">
+		 <div className="media-left">
 		   <img src={props.image.link} />
-		   {props.image.title}
+		 </div>
+		   <div className="media-body">
+		     <h4 className="media-heading">
+		       {props.image.title}
+		       </h4>
+		      </div>
 		</li>
 		);
 };
