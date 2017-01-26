13b28a271a28e3c8dd0acbca7e8dbb9d9e1624a7
MichaelWiss <michael.wiss@gmail.com>
2017-01-25 20:02:33 -0600

    ImageList component

---
+++ client/components/image_list.js
@@ -0,0 +1,17 @@
+// Create our image list component
+//import React
+import React from 'react';
+
+//create our component
+const ImageList = () => {
+   return (
+   	<ul>
+   	   <li>Image Detail</li> 
+   	</ul>
+   	);
+};
+
+
+//export our component
+
+export default ImageList;
--- client/main.js
+++ client/main.js
@@ -3,12 +3,14 @@
 //Import the react library
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ImageList from './components/image_list';
 
 //create a component
 const App = () => {
+
 	return (
 		<div>
-           React App #2
+		  <ImageList />
 		</div>
 	);
 };
