b6fa70fff0a5d3a207d5951cd4cb6452c6ab93cc
MichaelWiss <michael.wiss@gmail.com>
2017-01-26 17:03:31 -0600

    setState

--- client/main.js
+++ client/main.js
@@ -9,12 +9,20 @@ import ImageList from './components/image_list';
 
 //create a component
 class App extends Component{
-componentWillMount() {
+   constructor(props) {
+   	   super(props);
+
+   	   this.state = { images: [] };
+   }
+
+
+  componentWillMount() {
 	//fantastic place to load data
 	axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
-        .then(response => console.log(response));
+        .then(response => this.setState({ images: response.data.data }));
 }
 	render() {
+		console.log(this.state.images);
     return (
 		<div>
 		  <ImageList />
