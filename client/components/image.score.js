e5061aa403a3f8c5bfb3cea097660b04dd8a5db6
MichaelWiss <michael.wiss@gmail.com>
2017-01-26 19:20:08 -0600

    imagePercent constants

---
+++ client/components/image.score.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const ImageScore = () => {
+   //props.ups
+   //props.downs
+ const { ups, downs } = props;
+ //const ups = props.ups;
+// const downs = props.downs
+
+ const upsPercent = `${100 * (ups / (ups + downs))}%`;
+ const downsPercent = `${100 * (downs / (ups + downs))}%`;
+
+
+   return (
+       <div>
+        Ups/Downs
+	        <div className="progress">
+		       <div style={{ width: upsPercent}} className="progress-bar progress-bar-succes progress-bar-striped"/>
+		       <div style={{ width: downsPercent}}className="progress-bar progress-bar-danger progress-bar-striped"/>
+	       </div>
+       </div>
+   	);
+};
+
+
+
+export default ImageScore;
